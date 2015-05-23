e818ebbc2155fb29e2517810470acd9fe75dd977
Pedro Henrique <pedrohenriquerls@gmail.com>
2015-05-23 16:45:42 -0300

    mesh translate

--- app/models/point.rb
+++ app/models/point.rb
@@ -2,4 +2,11 @@
 class Point
   include Neo4j::ActiveNode
   property :name, type: String, index: :exact
+
+  def self.translate(mesh)
+    mesh_list = mesh.collect { |info| info.split(/\W+/) }
+    mesh_list.map do |info|
+    	{ from: info[0], to: info[1], distance: info[2].to_f }
+    end
+  end
 end
--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Point do
+	subject(:logistic_mesh) do
+		['A B 10', 'B D 15', 'A C 20', 'C D 30', 'B E 50', 'D E 30']
+	end
 
+	context '.translate' do
+		let(:translated_mesh) { Point.translate logistic_mesh }
+		it 'first mesh' do
+			mesh = translated_mesh.first
+
+			expect(mesh[:from]).to eq('A')
+			expect(mesh[:to]).to eq('B')
+			expect(mesh[:distance]).to eq(10.0)
+		end
+
+		it 'last mesh' do
+			mesh = translated_mesh.last
+
+			expect(mesh[:from]).to eq('D')
+			expect(mesh[:to]).to eq('E')
+			expect(mesh[:distance]).to eq(30.0)
+		end
+	end
 end
